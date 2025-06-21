a601d076d1c8a446421d0dfe540267f975e1ccd2
Bogdan Cazacu <contact@cazacubogdan.com>
2025-06-21 10:55:28 +0000

    adding vendore validation components#

--- src/openauthcert_tooling/__main__.py
+++ src/openauthcert_tooling/__main__.py
@@ -1,5 +1,6 @@
 import argparse
 from .validate_badge import validate_badge_main
+from .validate_vendor import validate_vendor_main
 
 def main():
     parser = argparse.ArgumentParser(description="OpenAuthCert Tooling CLI")
@@ -8,10 +9,15 @@ def main():
     badge_parser = subparsers.add_parser("validate-badge", help="Validate a badge JSON file")
     badge_parser.add_argument("file", help="Path to the badge JSON file")
 
+    vendor_parser = subparsers.add_parser("validate-vendor", help="Validate a vendor JSON file")
+    vendor_parser.add_argument("file", help="Path to the vendor JSON file")
+
     args = parser.parse_args()
 
     if args.command == "validate-badge":
         validate_badge_main(args.file)
+    elif args.command == "validate-vendor":
+        validate_vendor_main(args.file)
     else:
         parser.print_help()
 
